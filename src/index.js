267c716a21eb3ec3657759158c05c9f593d7e30d
iiio2 <gomexxxx71@gmail.com>
2021-04-04 18:37:49 +0600

    subscribe & unsubscribe.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import store from './store';
 
+// store.subscribe => it runs when the state changes everytime.
+
+const unsubscribe = store.subscribe(() => {
+  console.log('State Changed', store.getState());
+});
+
 store.dispatch({
   type: 'bugAdded',
   payload: {
     description: 'Bug1',
   },
 });
+
+// if it is called, after then nothing will be run in subscribe. it will not be notified;
+
+unsubscribe();
 
 store.dispatch({
   type: 'bugRemoved',
